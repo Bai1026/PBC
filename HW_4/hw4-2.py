3d87247428b9a2131a82374ed2d1d77eb58a85a0
白宗民 <zoungming@baizongmindeMacBook-Pro.local>
2024-03-31 23:21:01 +0800

    update hw4

--- HW_4/hw4-2.py
+++ HW_4/hw4-2.py
@@ -1,6 +1,7 @@
 def find_max_prob():
     max_prob = 0
-    client_count = 0 # since we can not use enumerate, so we use count += 1 instead
+    # since we can not use enumerate, so we use count += 1 instead
+    client_count = 0
     chosen_list = []
 
     for client in prob_list:
@@ -11,7 +12,8 @@ def find_max_prob():
 
         # since we have m+1 kinds of discount
         for i in range(discount_num + 1):
-            if limit_list[i] == 0: continue
+            if limit_list[i] == 0:
+                continue
 
             if client[i] >= max_prob:
                 if client[i] > max_prob:
@@ -22,6 +24,7 @@ def find_max_prob():
 
         client_count += 1
     return chosen_list
+
 
 def find_max_limit():
     max_limit = 0
@@ -39,18 +42,19 @@ def find_max_limit():
             max_limit_client_list.append((client+1, discount))
     return max_limit_client_list
 
+
 # client from 1 and discount from 0
-client_num, discount_num = map(int, input().split(','))
+client_num, discount_num = [int(x) for x in input().split(',')]
 
 # construct a 2D list, whose elements i is the prob of this client mapping each discount
 # notice that the 0th discount is no discount
 prob_list = []
 for _ in range(client_num):
-    tmp_list = list(map(int, input().split(',')))
+    tmp_list = [int(x) for x in input().split(',')]
     prob_list.append(tmp_list)
 
-limit_list = list(map(int, input().split(',')))
-block_list = list(map(int, input().split(',')))
+limit_list = [int(x) for x in input().split(',')]
+block_list = [int(x) for x in input().split(',')]
 
 # the 0th element has the limitation = client num
 limit_list.insert(0, client_num)
@@ -61,12 +65,13 @@ so I find the list with the "highest prob" first
 then I find the list with the "highest limitation num"
 then I find the list with the lowest "discount id"
 finally, print the list's first element's [0] and [1] since it's appended by the client id
-''' 
+'''
 
 max_prob_list = find_max_prob()
 max_limit_client_list = find_max_limit()
 
-min_discount = 9999999 # since we can not use float('inf), we use 99999 instead
+# since we can not use float('inf), we use 99999 instead
+min_discount = 9999999
 min_discount_list = []
 for item in max_limit_client_list:
     client, discount = item[0], item[1]
