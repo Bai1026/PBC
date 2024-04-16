63a763299f85943481f522fe579e2abd0c9ca82f
Bai1026 <1132abc30@gmail.com>
2024-04-16 21:36:35 +0800

    update hw6

--- HW_6/hw6-2.py
+++ HW_6/hw6-2.py
@@ -30,27 +30,51 @@ def construct_dict(input_file):
     return price_dict, num_dict
 
 
+# def find_store_or_product(id, price_dict, num_dict):
+#     if id in price_dict:
+#         return price_dict[id]
+
+      # if the key is not in price_dict and the key is string like "ZZ", this would crush the script
+#     elif int(id) in num_dict:
+#         return num_dict[int(id)]
+#     else:
+#         # return "BAD!!"
+#         raise KeyError()
+
 def find_store_or_product(id, price_dict, num_dict):
-    try:
-        if id in price_dict:
-            return price_dict[id]
-        elif id in num_dict:
-            return num_dict[id]
-    except KeyError as e:
-        print("BAD!!")
+    # check if the price_dict first, if exists -> return
+    if id in price_dict:
+        return price_dict[id]
+    else:
+        # try to transform into int, if is "ZZ" (not in price_dict and not convertible to int) -> Keyerror
+        try:
+            int_id = int(id)
+        except ValueError:
+            raise KeyError(f"ID {id} is not valid for num_dict and not found in price_dict.")
+
+        # if not, means it's convertible to int and check if is in the num_dict with int key
+        if int_id in num_dict:
+            return num_dict[int_id]
+        else:
+            # if id is int and not in the num_dict -> Keyerror still.
+            raise KeyError(f"ID {int_id} not found in num_dict.")
+
+
 
 def main():
     input_file = input()
     _ = input()
     # TYPE = input()
 
-    price_dict, num_dict = construct_dict(input_file)
-    print(price_dict, num_dict)
+    store_rev, product_sales_cnt = construct_dict(input_file)
+    print(store_rev, product_sales_cnt)
 
-    output = find_store_or_product('PB', price_dict, num_dict)
-    print(output)
+    try:
+        result = find_store_or_product('4', store_rev , product_sales_cnt)
+        print(result)
+    except KeyError as e:
+        print("BAD!!")
 
 
 if __name__ == '__main__':
     main()
-
